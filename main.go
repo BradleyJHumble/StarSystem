442f30896796118ee9e19d2157a428fc2dcf1ac1
judikial <bradleyjhumble@gmail.com>
2017-12-19 17:33:11 -0600

    Replaced Strings to Mux

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
+
 import (
-	"encoding/json"
-	"log"
-	"net/http"
-	"strings"
+    "encoding/json"
+    "log"
+    "net/http"
+    "github.com/gorilla/mux"
 )
+func main() {
+    router := mux.NewRouter()
+    log.Fatal(http.ListenAndServe(":8000", router))
+}
